fd75e02174c3f9cbafc4a94d1ee24df00b6ad06b
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-12 16:47:36 -0300

    finalizado jogo

--- js/game.js
+++ js/game.js
@@ -1,19 +1,41 @@
-// 1 - iniciar as variáveis
-
 let board = ['','','','','','','','',''];  // pode ser ou o, x ou ''
 let playerTime = 0; // jogador1 - 0; jogador2 - 1
+let gameOver = false;
 let symbols = ['o', 'x']; // se vez do jogador1 coloco o, se vez do jogador2 coloco x
-
+let winStates =[ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];  //mapeia as posições que dão vitória
 
 function handleMove(position){
+
+    if (gameOver){
+        return;
+    }
 
     if(board[position] == ''){
         board[position] = symbols[playerTime];
 
-        if (playerTime == 0){
-            playerTime = 1;
-        }else{
-            playerTime = 0;
+        gameOver = isWin();  // verifica se teve vencedor pós jogada
+
+        if(gameOver == false){
+            playerTime = (playerTime == 0) ? 1 : 0;
         }
     }
+    return gameOver;
 }
+
+function isWin(){
+
+    for(let i = 0; i < winStates.length; i++){
+        let seq = winStates[i];
+
+        let pos1 = seq[0];
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+        
+        if(board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != ''){
+            return true
+        }
+    }
+
+    return false;
+}
+
--- js/interface.js
+++ js/interface.js
@@ -5,30 +5,23 @@ document.addEventListener('DOMContentLoaded', () => {
     squares.forEach((square) => {
         square.addEventListener('click', handleClick);
     })
-
-})
+});
 
 function handleClick(event){
 
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
+    if(handleMove(position)){
+        setTimeout(() => {
+            alert(`O jogo acabou! O vencedor foi ${playerTime}`);
+        }, 20);
+    };
+    updateSquare(position);
+};
 
-}
-
-function updateSquares(){
-
-    let squares = document.querySelectorAll('.square')
-    
-    squares.forEach((square) => {
-        let position = square.id;
-        let symbol = board[position];
-
-        if(symbol != ''){
-            square.innerHTML = `<div class=${symbol}></div>`
-        }
-    })
-
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class=${symbol}></div>`
 }
