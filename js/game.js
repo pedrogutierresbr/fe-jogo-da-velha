d1348f4ddbe345f3ae06f816a11d65c8945c1878
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-13 12:10:54 -0300

    adicionado funcao restart ao jogo

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 let board = ['','','','','','','','',''];  // pode ser ou o, x ou ''
 let playerTime = 0; // jogador1 - 0; jogador2 - 1
 let gameOver = false;
+
 let symbols = ['o', 'x']; // se vez do jogador1 coloco o, se vez do jogador2 coloco x
 let winStates =[ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];  //mapeia as posições que dão vitória
 
--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,5 @@
+let players = [0x1F534,0x274C];
+
 document.addEventListener('DOMContentLoaded', () => {
 
     let squares = document.querySelectorAll('.square')
@@ -12,9 +14,10 @@ function handleClick(event){
     let square = event.target;
     let position = square.id;
 
+
     if(handleMove(position)){
-        setTimeout(() => {
-            alert(`O jogo acabou! O vencedor foi ${playerTime}`);
+        setTimeout(() => {  
+            alert(`O jogo acabou! \nO vencedor foi ${playerTime}`);
         }, 20);
     };
     updateSquare(position);
@@ -25,3 +28,19 @@ function updateSquare(position){
     let symbol = board[position];
     square.innerHTML = `<div class=${symbol}></div>`
 }
+
+function restartGame(){
+    board = ['','','','','','','','','']
+    playerTime = 0;
+    gameOver = false;
+    clearStage();
+}
+
+function clearStage(){
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.innerHTML = '';
+    });
+}
+
+document.getElementById('restart').onclick = restartGame;
